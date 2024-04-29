69615a157562d88addb942062cef4490186d5b84
yujia chen <yujia@yujiadeMacBook-Pro.local>
2024-04-28 18:28:38 -0700

    update output llm

--- utils.py
+++ utils.py
@@ -108,55 +108,65 @@ def create_dummy_agent(dummy_message):
 # 5. 如果用户的问题与提供的信息无关，请直接回答“不好意思，暂时没有找到相关问题的答案，正在为您转接人工客服。。。”。
 
 merge_results_prompt = PromptTemplate.from_template(
-#     """Based on the provided information, directly answer the user's specific question "{input}" in Chinese. Select only the information directly related to the question for your response, ignoring any irrelevant details.
+#     """Based on the information provided, directly answer the user's question "{input}". Follow these steps:
+# 1. The reference content includes several reference questions and their responses. Identify the most relevant question related to the user's query and retrieve its response, ignoring all other content.
+# 2. Summarize this one response and make the summary conversational, ignoring other reference content.
 
-#     Reference content: {context}
+# Reference Content: {context}
 
 # Guidelines:
 # 1. Carefully read the user's question to understand the specific requirements.
-# 2. Choose only the information directly relevant to the question from the provided context.
-# 3. Ensure the answer is direct, concise, and focused on solving the problem.
-# 4. Responses should be in clear Chinese.
-# 5. If the provided content does not contain sufficient information to address the question, clearly state so in Chinese.
+# 2. Identify only the most relevant response.
+# 3. The answer should mimic a human customer agent tone, be concise, conversational, polite, and focused on addressing the issue.
+# 4. Use clear Chinese.
+# 5. If the provided content does not contain sufficient information to resolve the question, clearly state this and suggest that the next step will be to connect with human customer service.
+# 6. Don't hallucinate.
 
 # Example:
 # Question: “专技个人登录时，提示验证失败”
-# Reference content: “登录为什么提示验证失败：请不要使用电脑自带和IE浏览器，建议使用谷歌、360浏览器（极速模式）、搜狗等浏览器，正常页面是可以拖动向右箭头完成滑块验证的。审核中，提示‘您的账号正在审核中，审核通过才可登录平台’：您可根据提示点击‘查看审核部门’，输入姓名、身份证号、密码进行查看待审核信息，需要审核通过后才可以登录。”
+# Reference Content: “问题：登录为什么提示验证失败    回复：请不要使用电脑自带和IE浏览器，建议使用谷歌、360浏览器（极速模式）、搜狗等浏览器，正常页面是可以拖动向右箭头完成滑块验证的。
 
-# The answer should be: “请不要使用电脑自带和IE浏览器，建议使用谷歌、360浏览器（极速模式）、搜狗等浏览器，正常页面是可以拖动向右箭头完成滑块验证的。”
+# 问题：审核中，提示‘您的账号正在审核中，审核通过才可登录平台    回复：您可根据提示点击‘查看审核部门’，输入姓名、身份证号、密码进行查看待审核信息，需要审核通过后才可以登录。”
+
+# Thinking steps:
+# 1. The most relavant reference question is "登录为什么提示验证失败"
+# 2. The most relavant reference question's response is "请不要使用电脑自带和IE浏览器，建议使用谷歌、360浏览器（极速模式）、搜狗等浏览器，正常页面是可以拖动向右箭头完成滑块验证的。"
+# 3. The output answer should be a verbal summarization of "请不要使用电脑自带和IE浏览器，建议使用谷歌、360浏览器（极速模式）、搜狗等浏览器，正常页面是可以拖动向右箭头完成滑块验证的。"
+# 4. The answer should be “您好，这个问题通常是因为使用电脑自带和IE浏览器，建议你可以使用谷歌、360浏览器（极速模式）、搜狗等浏览器，正常页面是可以拖动向右箭头完成滑块验证的。” 
 #     """
-    """根据提供的信息，直接回答用户的具体问题“{input}”。筛选出与问题直接相关的信息进行总结并回答，忽略不相关的部分。
+# 2. 从参考内容中选择并总结只与问题直接相关的信息，参考内容中一定存在不相关的部分！
 
+    """根据提供的信息，回答用户的提问“{input}”。遵循以下步骤：
+    1. 参考信息中包含若干个参考问题及其回复，通过匹配参考问题筛选出与用户的问题最相关的问题并获取其回复内容，忽略其他参考内容。
+    2. 对选取的回复进行总结并口语化总结，模仿中文人工客服的语气进行回答。
 
 参考内容：{context}
 
 指南：
-1. 仔细阅读用户问题，明确问题的具体需求。
-2. 从参考内容中选择并总结只与问题直接相关的信息。
-3. 回答模仿人类客服语气，应直接、精简、有礼貌，并且专注于解决问题。
-4. 模仿人类客服语气，
-5. 使用清晰的中文。
-6. 如果提供的内容中没有足够信息解决问题，应明确指出，并提出下一步会对接人工客服。
-7. 大部分情况下第一个回答是最准确的。
-
-例如：
-问题：“专技个人登录时，提示验证失败”
-参考内容：“登录为什么提示验证失败：请不要使用电脑自带和IE浏览器，建议使用谷歌、360浏览器（极速模式）、搜狗等浏览器，正常页面是可以拖动向右箭头完成滑块验证的。审核中，提示‘您的账号正在审核中，审核通过才可登录平台’：您可根据提示点击‘查看审核部门’，输入姓名、身份证号、密码进行查看待审核信息，需要审核通过后才可以登录。”
-
-回答应该是：“请不要使用电脑自带和IE浏览器，建议使用谷歌、360浏览器（极速模式）、搜狗等浏览器，正常页面是可以拖动向右箭头完成滑块验证的。”
+1. 只需要在参考内容中选择相关的部分进行总结，可以舍弃不相关的部分。
+2. 回答模仿人工客服语气，应精简、口语化、有礼貌。
+3. 如果提供的内容中没有足够信息解决问题，应明确指出，并提出下一步会对接人工客服。
     """
-    
-    # """ 你的任务是根据以下内容，用中文总结并回答用户的问题 “{input}”。
-    # 如果用户提供了反馈或建议，请从参考内容中提取最相关的回复话术，总结并回复。
 
-    # 参考内容：{context}
+# 例如：
+# 问题：“专技个人登录时，提示验证失败”
+# 参考内容：“问题：登录为什么提示验证失败    回复：请不要使用电脑自带和IE浏览器，建议使用谷歌、360浏览器（极速模式）、搜狗等浏览器，正常页面是可以拖动向右箭头完成滑块验证的。
+
+# 问题：审核中，提示‘您的账号正在审核中，审核通过才可登录平台  回复：您可根据提示点击‘查看审核部门’，输入姓名、身份证号、密码进行查看待审核信息，需要审核通过后才可以登录。”
+
+# 回答应该是：“请不要使用电脑自带和IE浏览器，建议使用谷歌、360浏览器（极速模式）、搜狗等浏览器，正常页面是可以拖动向右箭头完成滑块验证的。”
     
-    # 只需要在参考内容中选择相关的部分进行总结，可以舍弃不相关的部分。
-    # 不要添加任何新的信息，只需要总结原文的内容并回答问题。
-    # 不要提供任何个人观点或者评论。
-    # 不要产生幻觉。
-    # 回答尽可能的简单明了。
-    # 用中文回答问题。
+#     """ 你的任务是根据以下内容，用中文总结并回答用户的问题 “{input}”。
+#     如果用户提供了反馈或建议，请从参考内容中提取最相关的回复话术，总结并回复。
+
+#     参考内容：{context}
+    
+#     只需要在参考内容中选择相关的部分进行总结，可以舍弃不相关的部分。
+#     不要添加任何新的信息，只需要总结原文的内容并回答问题。
+#     不要提供任何个人观点或者评论。
+#     不要产生幻觉。
+#     回答尽可能的简单明了。
+#     用中文回答问题。
 # """
 )
 
@@ -169,7 +179,8 @@ merge_results_chain = LLMChain(
 )
 
 def merge_results(inp):
-    results, qa_map_path = inp['results'], inp['qa_map_path']
+    print('!!!!!!!!!!!!!!!input: ', inp)
+    results, qa_map_path, orig_question = inp['results']['tool_chain'], inp['qa_map_path'], inp['orig_question']
     with open(qa_map_path,'r') as f:
         qa_map = json.load(f)
 
@@ -181,7 +192,7 @@ def merge_results(inp):
     merged_answers = '\n\n'.join([qa_map[q] for q in question_list])
     print(merged_answers)
     print("input: ", RunnablePassthrough())
-    return {"context": RunnableLambda(lambda x: {"output": merged_answers}), "input": RunnablePassthrough()} | merge_results_chain
+    return {"context": RunnableLambda(lambda x: {"output": merged_answers}), "input": RunnableLambda(lambda x: orig_question)} | merge_results_chain
 
 def create_atomic_retriever_agent(tools, qa_map_path, system_prompt=None):
 
@@ -216,13 +227,17 @@ def create_atomic_retriever_agent(tools, qa_map_path, system_prompt=None):
         chosen_tool = tool_map[model_output["tool_use"]["name"]]
         if "qa_engine" in chosen_tool.name:
             def _parse_retreiver_inputs(model_output):
-                print("~~~~~:", model_output)
+                print("~~~~~1:", model_output)
                 return model_output["question"]["input"]
             def _parse_retreiver_outputs(model_output):
                 # model_output is a string with top k matches.
-                return {'results': model_output, 'qa_map_path': qa_map_path}
+                print("~~~~~2:", print(json.dumps(model_output, indent=4)))
+                return {'results': model_output, 'qa_map_path': qa_map_path, 'orig_question': model_output['question']['question']['input']}
             # print("~~~~~:", model_output)
-            return _parse_retreiver_inputs | chosen_tool | _parse_retreiver_outputs | RunnableLambda(merge_results) | output_parser_from_merged_results
+            run_chosen_tool = RunnableParallel(
+                question=RunnablePassthrough(),
+                tool_chain=_parse_retreiver_inputs | chosen_tool)
+            return run_chosen_tool | _parse_retreiver_outputs | RunnableLambda(merge_results) | output_parser_from_merged_results
             # return {"context": _parse_retreiver_inputs | chosen_tool, "input": RunnablePassthrough()} | summarization_llm_prompt | summarization_llm | output_parser
         
         return {"params": RunnableLambda(lambda x: x["tool_use"]["arguments"])} | chosen_tool | output_parser
